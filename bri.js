6bb290de6716d0af5420de124147a95c09b99051
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-06-20 23:44:11 +0300

    Update bri.js

---
+++ bri.js
@@ -0,0 +1,333 @@
+
+  "request_date_ms": 1704954622267,
+  "request_date": "2024-01-11T06:30:22Z",
+  "subscriber": {
+    "last_seen": "2024-01-11T06:28:01Z",
+    "original_application_version": "53942",
+    "first_seen": "2024-01-11T06:28:01Z",
+    "subscriptions": {
+      "UNFOLD_FOR_BRANDS_YEAR": {
+        "original_purchase_date": "2024-01-11T06:28:38Z",
+        "expires_date": "2099-01-18T06:28:37Z",
+        "is_sandbox": false,
+        "refunded_at": null,
+        "store_transaction_id": "460001766854464",
+        "auto_resume_date": null,
+        "grace_period_expires_date": null,
+        "period_type": "trial",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "billing_issues_detected_at": null,
+        "ownership_type": "PURCHASED",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      }
+    },
+    "other_purchases": {},
+    "management_url": "https://apps.apple.com/account/subscriptions",
+    "entitlements": {
+      "BIRTHDAY1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PC1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS3_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "RP1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS2_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "RP1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "LOVE3_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "TUTORIAL1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "LOVE2_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS4_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "JN1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS2_AN_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "LOVE1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "RP2_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "RP2_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "TUTORIAL1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS3_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "SB1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PL1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PRO_YEARLY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "DW1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "JN2_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FF1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "MOODBOARD_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "RP3_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS4_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FF2_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "LOVE3_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FF1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "DW2_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "RED1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "SB1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "BESTDRESSED1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "ZD1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FF2_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FF3_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "JN1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PROMOTION1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "DW1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PRIDE1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "MOODBOARD1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "EFFECTS1_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS2_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FF3_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PL1_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "JN2_POST": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "FS1_FONT": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "PRO": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "DW2_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      },
+      "CS1_AN_STORY": {
+        "expires_date": "2099-01-18T06:28:37Z",
+        "purchase_date": "2024-01-11T06:28:37Z",
+        "product_identifier": "UNFOLD_FOR_BRANDS_YEAR",
+        "grace_period_expires_date": null
+      }
+    },
+    "original_purchase_date": "2024-01-11T06:27:28Z",
+    "original_app_user_id": "$RCAnonymousID:c1dd56df4c71424fa3ba1963bd21899b",
+    "non_subscriptions": {}
+  }
+}
