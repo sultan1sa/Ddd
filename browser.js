25612c278996f70804aacc4a6dc26d226b61892b
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-06-20 23:34:18 +0300

    Update browser.js

---
+++ browser.js
@@ -0,0 +1,66 @@
+{
+  "request_date_ms": 1715232835861,
+  "request_date": "2024-05-09T05:33:55Z",
+  "subscriber": {
+    "non_subscriptions": {
+      "com.overdesigned.incognito.lifetime": [
+        {
+          "id": "a2e80e6a43",
+          "is_sandbox": true,
+          "purchase_date": "2023-12-25T20:41:56Z",
+          "original_purchase_date": "2023-12-25T20:41:56Z",
+          "store": "app_store",
+          "store_transaction_id": "2000000487324972"
+        },
+        {
+          "id": "d1ef312e90",
+          "is_sandbox": true,
+          "purchase_date": "2024-02-09T07:01:12Z",
+          "original_purchase_date": "2024-02-09T07:01:12Z",
+          "store": "app_store",
+          "store_transaction_id": "2000000520358890"
+        },
+        {
+          "id": "cf8a02e3dc",
+          "is_sandbox": true,
+          "purchase_date": "2024-03-05T05:31:47Z",
+          "original_purchase_date": "2024-03-05T05:31:47Z",
+          "store": "app_store",
+          "store_transaction_id": "2000000539953882"
+        }
+      ]
+    },
+    "first_seen": "2023-12-25T20:40:58Z",
+    "original_application_version": "1.0",
+    "other_purchases": {
+      "com.overdesigned.incognito.lifetime": {
+        "purchase_date": "2024-03-05T05:31:47Z"
+      }
+    },
+    "management_url": null,
+    "subscriptions": {
+      "$rc_annual": {
+        "store": "app_store",
+        "purchase_date": "2023-09-09T09:09:09Z",
+        "product_identifier": "",
+        "original_purchase_date": "2023-09-09T09:09:09Z",
+        "ownership_type": "PURCHASED"
+      }
+    },
+    "entitlements": {
+      "com.overdesigned.ShareBear.AnnualSubscription": {
+        "product_identifier": "$rc_annual",
+        "purchase_date": "2023-09-09T09:09:09Z"
+      },
+      "unlock": {
+        "grace_period_expires_date": null,
+        "purchase_date": "2024-03-05T05:31:47Z",
+        "product_identifier": "
+        "expires_date": null
+      }
+    },
+    "original_purchase_date": "2013-08-01T07:00:00Z",
+    "original_app_user_id": "$RCAnonymousID:4d730e5deb6b4b73bcefe0ddc81dfb18",
+    "last_seen": "2024-05-08T18:14:36Z"
+  }
+}
