d0ab047935cc849b06e684eb1abe9747af6ed6cc
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-07-29 18:11:05 +0300

    Update incognito2.js

--- test/incognito2.js
+++ test/incognito2.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/AnimeArt.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/AnimeArt.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Ddd/main/test/incognito2.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/sultan1sa/Ddd/main/test/incognito2.js
 
 [mitm]
 hostname = api.revenuecat.com
